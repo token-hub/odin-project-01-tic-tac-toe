51be66d8136887adac94ce9fe1001971eea48d09
John <johnsuyang2118@gmail.com>
2024-08-08 19:15:18 +0800

    Added initial objects and tasks to do

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log("Hello");
+const gameboard = (function () {
+    // start / restart game
+    return {};
+})();
+
+// player x and player o
+const player = (function () {
+    // add mark in the board
+    // add player name
+    return {};
+})();
+
+// * task 1
+// when the player clicks the start button
+// then 3x3 box will appear
+
+// * task 2
+// shows which player is current playing. starts with player x
+
+// * task 3
+// mark a box with the current player.
+
+// * task 4
+// add a check that only boxes that is not marked by a player will be mark
+
+// * task 5
+// check if there's boxes that contains 3 same mark that you can draw a line
+// show which player wons then show the restart button
+
+// * task 6
+// check if all the boxes are already marked and the game is still not ended
+// -- show restart the game button.
